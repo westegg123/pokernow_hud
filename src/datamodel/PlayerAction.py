5162949f84dfc616d31b9fcccdc3a2781ed01824
Andreas Komissopoulos <westegg123@gmail.com>
2020-07-27 22:55:42 -0500

    Add skeleton for amount as part of PlayerAction

--- src/datamodel/PlayerAction.py
+++ src/datamodel/PlayerAction.py
@@ -1,10 +1,13 @@
 class PlayerAction:
     _player = ""
     _action = ""
+    _amount = ""
 
+    ##todo: andkom - add amount to player action
     def __init__(self, player, action):
         self._player = player
         self._action = action
+        self._amount = ""
 
     def action(self):
         return self._action
@@ -12,5 +15,9 @@ class PlayerAction:
     def player(self):
         return self._player
 
+    def amount(self):
+        return self._amount
+
     def to_string(self):
-        return self.player() + ": " + self.action()
+        amount = "" if self.amount() is "" else ":" + self.amount()
+        return self.player() + ": " + self.action() + amount
