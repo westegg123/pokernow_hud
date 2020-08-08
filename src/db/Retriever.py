f81117dadb60bd355dfcfa57f2c0ed82feef40a1
Andreas Komissopoulos <westegg123@gmail.com>
2020-08-08 02:00:29 -0500

    Add retriever of player statistics

---
+++ src/db/Retriever.py
@@ -0,0 +1,11 @@
+from db.DatabaseObjectConverter import DatabaseObjectConverter
+
+class Retriever:
+    _client = ""
+
+    def __init__(self, client):
+        self._client = client
+
+    def retrieve_player_statistics(self, player_name):
+        query = { "player" : player_name}
+        return self._client.db().player_statistics.find_one(query) ## todo: andkom - extract constant to file (player_statistics)
