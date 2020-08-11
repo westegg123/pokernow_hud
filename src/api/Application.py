8588101d9cd42b80505aba0ba3dd13802d987895
Andreas Komissopoulos <westegg123@gmail.com>
2020-08-10 21:36:02 -0500

    Allow CORS (todo: increased granularity)

--- src/api/Application.py
+++ src/api/Application.py
@@ -9,9 +9,13 @@ class Application:
         self._retriever = retriever
 
         ## Routes
-        @self._app.route('/statistics/<player_name>', methods=["GET"])
+        @self._app.route('/api/statistics/<player_name>', methods=["GET"])
         def get_all_statistics(player_name):
-            return json_util.dumps(self._retriever.retrieve_player_statistics(player_name))
+            dumps = json_util.dumps(self._retriever.retrieve_player_statistics(player_name))
+            response = flask.Response(dumps)
+            response.headers['Access-Control-Allow-Origin'] = '*' ##todo: andkom - better access restriction
+            print(response)
+            return response
 
     def start(self):
         return self._app.run()
