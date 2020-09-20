f68f0cfa41a7950d7a63717e3d83231d5305dd85
Andreas Komissopoulos <westegg123@gmail.com>
2020-09-20 18:45:12 -0500

    Allow Querying All PlayerStatistics at Once

--- src/api/Application.py
+++ src/api/Application.py
@@ -10,10 +10,19 @@ class Application:
 
         ## Routes
         @self._app.route('/api/statistics/<player_name>', methods=["GET"])
-        def get_all_statistics(player_name):
+        def get_all_statistics_for_player(player_name):
             dumps = json_util.dumps(self._retriever.retrieve_player_statistics(player_name))
             response = flask.Response(dumps)
             response.headers['Access-Control-Allow-Origin'] = '*' ##todo: andkom - better access restriction
+            print(response)
+            return response
+
+        @self._app.route('/api/statistics/all', methods=["GET"])
+        def get_all_statistics():
+            all_stats = self._retriever.retrieve_all_player_statistics()
+            dumps = json_util.dumps(self._retriever.retrieve_all_player_statistics())
+            response = flask.Response(dumps)
+            response.headers['Access-Control-Allow-Origin'] = '*'  ##todo: andkom - better access restriction
             print(response)
             return response
 
--- src/db/Retriever.py
+++ src/db/Retriever.py
@@ -9,3 +9,6 @@ class Retriever:
     def retrieve_player_statistics(self, player_name):
         query = { "player" : player_name}
         return self._client.db().player_statistics.find_one(query) ## todo: andkom - extract constant to file (player_statistics)
+
+    def retrieve_all_player_statistics(self):
+        return self._client.db().player_statistics.find()
